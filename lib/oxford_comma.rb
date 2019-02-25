10ccfb2c2066a6ae46acd5f6f6bf68fa82381938
Brian Simonton <bsimonton78@gmail.com>
2019-02-25 18:11:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,18 @@
 def oxford_comma(array)
 
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  
+  elsif array.length == 1
+  	array.join
+  	
+  elsif array.length > 2
+    array[-1] << "and "
+	array.join(", ")
+  end
+  
+
 
 
 
